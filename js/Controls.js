e9578b46f6f2b9000f507233ef39b493b524e7db
Raine Virta <raine.virta@gmail.com>
2019-03-03 13:01:42 +0200

    Controls: remove is-scrolled-top thing

--- js/Controls.js
+++ js/Controls.js
@@ -1,8 +1,6 @@
-import React, { useEffect, useState } from 'react'
-import classNames from 'classnames'
+import React from 'react'
 import Dropdown from './dropdown'
 import '../css/Controls.scss'
-import K from 'kefir'
 
 const TIMEFRAMES = [
   { key: '1h', text: '1h' },
@@ -15,40 +13,24 @@ const CHANNEL_SELECTION = [
   { key: 'POPULAR', text: 'popular channels' }
 ]
 
-const Controls = React.memo(({ dispatch, timeframe, channelListType }) => {
-  const [isScrolledTop, setIsScrolledTop] = useState(true)
-
-  useEffect(() => {
-    const s = K.fromEvents(document, 'scroll')
-      .throttle(20)
-      .map(() => document.scrollingElement.scrollTop === 0)
-      .skipDuplicates()
-
-    s.onValue(setIsScrolledTop)
-    return () => s.offValue(setIsScrolledTop)
-  }, [])
-
-  return (
-    <div
-      className={classNames('controls', { 'is-scrolled-top': isScrolledTop })}
-    >
-      <div className="channel-selector">
-        <Dropdown
-          options={CHANNEL_SELECTION}
-          value={channelListType}
-          setValue={(value) => dispatch({ type: 'setChannelListType', value })}
-        />
-      </div>
-      <div className="timeframe-selector">
-        <Dropdown
-          options={TIMEFRAMES}
-          value={timeframe}
-          setValue={(value) => dispatch({ type: 'setTimeframe', value })}
-        />
-      </div>
+const Controls = React.memo(({ dispatch, timeframe, channelListType }) => (
+  <div className="controls">
+    <div className="channel-selector">
+      <Dropdown
+        options={CHANNEL_SELECTION}
+        value={channelListType}
+        setValue={(value) => dispatch({ type: 'setChannelListType', value })}
+      />
     </div>
-  )
-})
+    <div className="timeframe-selector">
+      <Dropdown
+        options={TIMEFRAMES}
+        value={timeframe}
+        setValue={(value) => dispatch({ type: 'setTimeframe', value })}
+      />
+    </div>
+  </div>
+))
 
 Controls.displayName = 'Controls'
 
